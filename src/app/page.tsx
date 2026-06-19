054d934e45eb974100ad1cbcfc294ae02370929e
adrianl01 <ga.leiva.1601@gmail.com>
2026-06-19 09:55:58 -0300

    refactor Page component to use MapScreen and remove FeedScreen and SplashScreen logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 "use client";
-import FeedScreen from '../components/screens/FeedScreen';
-import SplashScreen from '../components/screens/SplashScreen';
-import { getUserLocation } from '../lib/storage/location';
-import { useRouter } from 'next/navigation';
+import MapScreen from '../components/screens/MapScreen';
 
 export default function Page() {
-  const router = useRouter();
 
-  const userLocation = getUserLocation();
-
-  if (userLocation !== null) return <FeedScreen />;
-  return <SplashScreen />;
+  return <MapScreen />;
 }
