08b6608b7cca84494d21e8ce1e6ff716ba9663e7
adrianl01 <ga.leiva.1601@gmail.com>
2026-06-19 11:33:44 -0300

    update Page component to redirect to /map and remove unnecessary comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-// "use client";
-// import { redirect } from 'next/navigation';
+"use client";
+import { redirect } from 'next/navigation';
 
-
-// export default function Page() {
-//   redirect("/map")
-// }
 
 export default function Page() {
-  return <h1>Hola desde Next.js</h1>;
+  redirect("/map")
 }
+
