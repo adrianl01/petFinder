cfba5b809ec666f98056d0285e8abdb8916c30fc
adrianl01 <ga.leiva.1601@gmail.com>
2026-06-19 11:09:21 -0300

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-"use client";
-import { redirect } from 'next/navigation';
+// "use client";
+// import { redirect } from 'next/navigation';
 
+
+// export default function Page() {
+//   redirect("/map")
+// }
 
 export default function Page() {
-  redirect("/map")
+  return <h1>Hola desde Next.js</h1>;
 }
