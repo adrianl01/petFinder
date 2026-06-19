90b39d325242f17b31ad08af2f5aec698bf94612
adrianl01 <ga.leiva.1601@gmail.com>
2026-06-19 09:28:51 -0300

    remove Header, AtentionCard, Location components and associated styles

--- src/hooks/useReports.ts
+++ src/hooks/useReports.ts
@@ -1,36 +1,22 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-import { getReportsNear } from "@/src/lib/api/reports";
+import { getReportsNearMe } from '@/src/lib/api/reports';
 
-export function useReports(
-  lat?: number,
-  lng?: number
-) {
-  const [reports, setReports] =
-    useState<any[]>([]);
+export function useReports(lat?: number, lng?: number) {
+  const [reports, setReports] = useState<any[]>([]);
 
-  const [loading, setLoading] =
-    useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (
-      lat === undefined ||
-      lng === undefined
-    )
-      return;
+    if (lat === undefined || lng === undefined) return;
 
-    getReportsNear(
-      lat,
-      lng
-    )
+    getReportsNearMe()
       .then((reports) => setReports(reports as any[]))
-      .finally(() =>
-        setLoading(false)
-      );
+      .finally(() => setLoading(false));
   }, [lat, lng]);
 
   return {
     reports,
-    loading,
+    loading
   };
 }
